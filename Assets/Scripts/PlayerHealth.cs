ca200077584ec17139eb327c92fb914c242860c8
Nathan Gardea <natecgardea@gmail.com>
2023-08-17 20:34:56 -0700

    Working Heart system on UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,9 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : DamageableEntity
 {
+    public float numOfHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        if (hp > numOfHearts)
+        {
+            numOfHearts = hp;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < hp)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                hearts[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
     public override void Die()
     {
         Debug.Log("YOU DIED");
