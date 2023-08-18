d80efe6f7aa4bf6663516797f27df36d413b9f6b
Nathan Gardea <natecgardea@gmail.com>
2023-08-17 18:55:10 -0700

    Main Title

--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -6,14 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelControl : MonoBehaviour
 {
-    public void PlayAgain()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
     public void ReturnToTitle()
     {
-        Debug.Log("Returning to the map");
-        SceneManager.LoadScene("Title Screen");
+        Debug.Log("Returning to the menu");
+        SceneManager.LoadScene("Main Menu");
     }
 }
